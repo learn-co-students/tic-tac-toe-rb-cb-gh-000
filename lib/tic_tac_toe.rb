1d1a45416146e112362e5aafdc87f596c42cb543
oussama kahloul <oussama.kahloul.1996@gmail.com>
2017-02-14 14:38:15 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,109 @@
 
+# Define your WIN_COMBINATIONS constant
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+
+    return combination if position_taken?(board, combination[0]) && board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]]
+  end
+
+false
+end
+
+
+def full?(board)
+  !(board.include?(" ") || board.include?(nil))
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+  (combination = won?(board)) ? board[combination[0]] : nil
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def turn_count(board)
+  conter = 0
+  board.each do | element |
+if element != " "
+
+  conter += 1
+
+end
+end
+conter
+  end
+
+
+  def current_player(board)
+    if turn_count(board) % 2 == 0
+  board = "X"
+else board = "O"
+
+end
+end
+def play(board)
+    until   over?(board) 
+    
+turn(board)
+      
+    end   
+    if won?(board)
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cats Game!"
+      
+    end
+end

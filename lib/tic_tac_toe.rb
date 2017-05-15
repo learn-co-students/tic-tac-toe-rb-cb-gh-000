b16aa3ed802d3b61665c1b69a4bbd5d79ff76f7f
Dyl Bromell <dyl@resonategroup.ca>
2017-05-15 19:22:46 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,119 @@
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8)
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2], # H1
+  [3,4,5], # H2
+  [6,7,8], # H3
+  [0,3,6], # V1
+  [1,4,7], # V2
+  [2,5,8], # V3
+  [0,4,8], # D1
+  [6,4,2]  # D2
+]
+
+def won?(board)
+  # Iterate through win condition array for X and O
+  WIN_COMBINATIONS.each do |win_combination|
+    # For each potential win combination, iterate and check if
+    # all indices of combination are filled by all X or all O
+    winning_x = win_combination.all? do |index|
+      board[index].include?("X")
+    end
+    winning_o = win_combination.all? do |index|
+      board[index].include?("O")
+    end
+    # Return winning combination from array as soon as matching
+    # X or O combination is found
+    if winning_x
+      return win_combination
+    elsif winning_o
+      return win_combination
+    end
+  end
+  # If no win conditions are met, return false
+  false
+end
+
+def full?(board)
+  board.all? do |i|
+    i == "X" || i == "O"
+  end
+end
+
+def draw?(board)
+  (!won?(board) && full?(board))
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  winning_combo = won?(board)
+  if won?(board)
+     board[winning_combo[0]]
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(array, index, value)
+  array[index] = value
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |index|
+    if index.include?("X") || index.include?("O")
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  if (turn_count(board) % 2 == 0)
+    "X"
+  else
+    "O"
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end

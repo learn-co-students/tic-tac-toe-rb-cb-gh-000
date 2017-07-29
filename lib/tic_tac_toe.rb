0286a9d82c6e8635b57f134f8544153f139efcea
Hiram Torres <javier@scelabs.com>
2017-07-29 01:20:59 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,118 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i
+  index - 1
+end
+
+def move(array, index, value)
+  array[index] = value
+end
+
+def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.chomp
+    index = input_to_index(input)
+
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  turn.even? ? "X" : "O"
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 != nil && position_1 != "" && position_1 != " " && position_1 == position_2 && position_1 == position_3
+      return win_combination
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |item|
+    item == "X" || item == "O"
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if item = won?(board)
+    return board[item[0]]
+  end
+end
+
+def play(board)
+  display_board(board)
+
+  until over?(board)
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end

59b4230e36ed3eb3b5f13379ecadde3aa917c259
Michelle Gouw <m.gouw999@gmail.com>
2017-08-10 08:19:34 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,125 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input = user_input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
+
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  index.between?(0, 8) && (position_taken?(board, index) == false)
+end
+
+#code your turn method here
+def turn(board)
+  puts "Please enter 1-9:"
+  user_num = gets.to_i
+  index = input_to_index(user_num)
+  if valid_move?(board, index)
+    board[index] = "X"
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+#define your tur_count method here
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space != " "
+      count += 1
+    end
+  end
+  return count
+end
+
+#current_player method
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+# Define your won? method below
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    index1 = combination[0]
+    index2 = combination[1]
+    index3 = combination[2]
+    if position_taken?(board, index1) && position_taken?(board, index2) && position_taken?(board, index3)
+      if (board[index1] == board[index2]) && (board[index2] == board[index3])
+        return combination
+      end
+    end
+  end
+  return false
+end
+
+def full?(board)
+   board.all? do |position|
+    position == "O" || position == "X"
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if draw?(board) || !over?(board)
+    return nil
+  else
+    return board[won?(board)[0]]
+  end
+end
+
+# Define your play method below
+def play(board)
+  9.times do
+    turn(board)
+  end
+end

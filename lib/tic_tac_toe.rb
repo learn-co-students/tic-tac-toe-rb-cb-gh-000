8fa6448351d0f713edef6eb21e19d6d90dd0e46b
Erwan Mailharro <erwan.mailharro@gmail.com>
2017-09-26 11:14:33 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,110 @@
 
+# Helper Methods
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index,current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+def play(board)
+  until over?(board) do
+    turn(board)
+  end
+  if draw?(board)
+    puts "Cat's Game!"
+  else
+    puts "Congratulations #{winner(board)}!"
+  end
+
+end
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+
+]
+
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    if position_taken?(board,combination[0]) && board[combination[0]] == board[combination[1]] && board[combination[1]] == board[combination[2]]
+      return combination
+    end
+  end
+
+  return nil
+end
+
+def full?(board)
+  board.all? do |element|
+    element == "X" || element =="O"
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  draw?(board) || won?(board)
+end
+
+def winner(board)
+    if won?(board)
+      return board[won?(board)[0]]
+    end
+end
+
+def turn_count(board)
+  turn = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turn +=1
+    end
+  end
+  turn
+end
+
+def current_player(board)
+  if turn_count(board)%2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end

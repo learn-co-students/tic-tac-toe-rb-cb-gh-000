d8ed6ad92e4b66783d74a7d4717cacb2201d8ef9
Zhi Lu <luzhi92@gmail.com>
2017-10-07 05:40:49 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,119 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, player)
+  board[index] = player
+end
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def valid_move?(board, index)
+  !position_taken?(board, index) && ((0...board.size) === index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  player = current_player(board)
+  if valid_move?(board, index)
+    move(board, index, player)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |value|
+    if value != " "
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+def comb_won?(board, comb)
+  comb.all?{|i| board[i] == "O"} || comb.all?{|i| board[i] == "X"}
+end
+
+def won?(board)
+  won = false
+  WIN_COMBINATIONS.each do |comb|
+    if comb_won?(board, comb)
+      won = true
+      return comb
+    end
+  end
+  if !won
+    return nil
+  end
+end
+
+def full?(board)
+  board.all?{|i| i == "O" || i == "X"}
+end
+
+def draw?(board)
+  if !won?(board) and full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  won = won?(board)
+  if won
+    return board[won[0]]
+  else
+    return nil
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if winner(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+
+end

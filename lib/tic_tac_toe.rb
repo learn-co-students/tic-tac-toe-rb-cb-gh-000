e86184565965addff8c74e068aad6328b1d9fa41
Zachary Wolner <zachary.wolner@gmail.com>
2017-12-22 18:42:02 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,101 @@
+def play(board)
+  until over?(board) || draw?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board).upcase}!"
+  else draw?(board)
+    puts "Cat's Game!"
+  end
+end
 
+#Display board method
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+#position taken method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def input_to_index(user_input)
+  index = (user_input.to_i - 1)
+end
+
+def move(board, index, player_token)
+  board[index] = player_token
+end
+
+def valid_move?(board, index)
+   !(position_taken?(board, index)) && (0..9).include?(index)
+end
+
+def turn(board)
+  puts "Please enter a move 1-9:"
+  player_token = current_player(board)
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board,index,player_token)
+    display_board(board)
+  else
+    turn(board)
+  end
+end

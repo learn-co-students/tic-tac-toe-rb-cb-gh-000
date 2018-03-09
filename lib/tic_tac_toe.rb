4b923d4259fb2664e24de76ba8b9b80dd96b7821
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-09 02:05:45 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,109 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |place|
+    if place == 'X' or place == 'O'
+      counter += 1
+    end
+  end
+
+  return counter
+end
+
+def current_player(board)
+  number_turn = turn_count(board) + 1
+  return number_turn % 2 != 0 ? 'X' : 'O'
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+end
+
+def full?(board)
+  board.all?{|place| place != " "}
+end
+
+def draw?(board)
+  !(won?(board)) && (full?(board))
+end
+
+def over?(board)
+  (won?(board)) || (full?(board)) || (draw?(board))
+end
+
+def winner(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return "X"
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
+    end
+  end
+end
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end

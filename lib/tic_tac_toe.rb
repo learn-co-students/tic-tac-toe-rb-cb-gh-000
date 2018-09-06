4c9c0a824ffd2b1f6a80cf17078a20b39823de2d
fusion MO <pzj1218331400@gmail.com>
2018-09-06 13:04:13 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,55 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def player_move(board, index, symbol)
+  board[index] = symbol
+end
+
+def position_taken?(board, position)
+  if board[position] == " "
+    return false
+  elsif board[position] == ""
+    return false
+  elsif board[position] == nil
+    return false
+  elsif board[position] == "X" || board[position] == "O"
+    return true
+  end
+end
+
+def valid_move?(board, position)
+  if position > 8 || position < 0
+    return false
+  else
+    !position_taken?(board, position)
+  end
+end
+
+def turn(board)
+  user_input = gets.strip
+  position = input_to_index(user_input)
+  if valid_move?(position) == true
+    player_move(board, index, symbol)
+  else
+    turn(board)
+end

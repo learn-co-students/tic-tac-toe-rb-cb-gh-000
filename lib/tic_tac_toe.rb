4fa0b5701bc6e2610838be07e2a9e42476dc05fb
Reece Humphreys <reecewh@icloud.com>
2018-12-22 00:10:41 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,104 @@
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  # Horizontal Wins
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  # Vertical Wins
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  # Diagonal Wins
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.find do |win|
+    if board[win[0]] != "" && board[win[0]] != " " && board[win[0]] == board[win[1]] && board[win[1]] == board[win[2]]
+      return win
+    end
+  end
+end
+
+def full?(board)
+  board.none? { |space| space == "" || space == " " }
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || won?(board) || draw?(board)
+end
+
+def winner(board)
+  winner = won?(board)
+	  if winner != nil
+	    winner_index = winner[0]
+	    return board[winner_index]
+	  end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if !(space == "" || space == " " || space == nil)
+      turns +=1
+    end
+  end
+  turns
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  valid_move = false
+  until valid_move
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    valid_move = valid_move?(board, index)
+  end
+  move(board, index, current_player(board))
+  display_board(board)
+end
+
+def play(board)
+  until over?(board) || draw?(board)
+    turn(board)
+  end
+  if winner(board) != nil
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end

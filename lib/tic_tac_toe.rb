0ba31479cce1490f0ecbef7dd464fc2593d9d683
Ben Francisco <bpfranci@math.tamu.edu>
2019-08-19 22:39:03 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,14 @@
+# global constant for winning combinations
+WIN_COMBINATIONS = [
+  # row combinations
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  # column combinations
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  # diagonal combinations
+  [0,4,8],
+  [2,4,6]
+]
